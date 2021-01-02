9125711f96134c582a9842e9741e87122848ee3f
Mark N Hernandez <marknhernandez@icloud.com>
2021-01-01 18:24:22 -0600

    first commit

---
+++ Assim.py
@@ -0,0 +1,27 @@
+def Data(filename): 
+    with open(filename, 'r') as g:
+        numbers = g.readlines()
+        data = dict(enumerate(numbers))
+    title = filename[38:-8]
+    month = data[58][5:7]
+    day = data[58][8:10]
+    year = data[58][:4]
+    date = "{}/{}/{}".format(month, day, year)
+    row1 = data[119].strip()
+    row2 = data[121].strip()
+    row3 = data[123].strip()
+    package = "{}, {}, {}, {}, {}\n".format(title,date,row1,row2,row3)
+    #print(package)
+    fo = open('data.txt', 'a')
+    fo.write(package)
+    fo.close
+
+with open('dir.txt', 'r') as d:
+    for i in d:
+        name = i.strip("\n")
+        #print(name)
+        if (name.find('Blank') != -1):
+            pass
+        else:
+            Data(name)
+
